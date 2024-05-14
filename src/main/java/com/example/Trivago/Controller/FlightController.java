184423293df0fd4bc506f4d6d31391090ca09a17
abrilramos12 <abrilramos7@hotmail.com>
2024-05-14 13:31:14 -0300

    update proyect

--- src/main/java/com/example/Trivago/Controller/FlightController.java
+++ src/main/java/com/example/Trivago/Controller/FlightController.java
@@ -1,7 +1,8 @@
 package com.example.Trivago.Controller;
-
+import com.example.Trivago.DTO.FlightDTO;
 import com.example.Trivago.Service.IFlight;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,4 +16,20 @@ public class FlightController {
 
     @Autowired
     IFlight flightService;
+
+
+    @GetMapping("/flights")
+    public ResponseEntity<?> getAllHotels() {
+        try {
+            List<FlightDTO> flights = flightService.getAll();
+
+            if (flights.isEmpty()) {
+                return new ResponseEntity<>("No existen vuelos", HttpStatus.NO_CONTENT);
+            } else {
+                return new ResponseEntity<>(flights, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error inesperado en vuelos", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+}
--- src/main/java/com/example/Trivago/Service/IFlight.java
+++ src/main/java/com/example/Trivago/Service/IFlight.java
@@ -1,5 +1,10 @@
 package com.example.Trivago.Service;
 
+import com.example.Trivago.DTO.FlightDTO;
+
+
+import java.util.List;
+
 public interface IFlight {
-    
+    List<FlightDTO> getAll();
 }
