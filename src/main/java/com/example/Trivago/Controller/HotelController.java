5d4575877eec71d9893f0d45cbb48a8cd5e1174f
Imanol Martinez <imanolmartinez249@gmail.com>
2024-05-20 14:43:42 -0300

    Merge branch 0001

--- src/main/java/com/example/Trivago/Controller/HotelController.java
+++ src/main/java/com/example/Trivago/Controller/HotelController.java
@@ -23,16 +23,7 @@ public class HotelController {
 
     @GetMapping("/hotels")
     public ResponseEntity<?> getAllHotels() {
-        try {
-            List<HotelDTO> hotels = hotelService.getAll();
-
-            if (hotels.isEmpty()) {
-                return new ResponseEntity<>("No existen hoteles", HttpStatus.NO_CONTENT);
-            } else {
-                return new ResponseEntity<>(hotels, HttpStatus.OK);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error inesperadoq", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        List<HotelDTO> hotels = hotelService.getAll();
+        return ResponseEntity.ok(hotels);
     }
 }
